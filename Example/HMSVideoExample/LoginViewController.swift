e106a7eb5924a5e8810a382bea60cb6186bd90d4
Yogesh Singh <yogesh@100ms.live>
2021-02-26 15:07:23 +0530

    updated login view

--- Example/HMSVideoExample/LoginViewController.swift
+++ Example/HMSVideoExample/LoginViewController.swift
@@ -11,52 +11,11 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var roomNameField: UITextField!
     @IBOutlet weak var userNameField: UITextField!
     
+    @IBOutlet weak var backgroundView: UIImageView!
+    
     // MARK: - View Lifecycle
     
-    override func viewDidLoad() {
-        addNavBarImage()
-        
-        NotificationCenter.default.addObserver(forName: UIDevice.orientationDidChangeNotification,
-                                               object: nil,
-                                               queue: .main) { (notification) in
-            self.addNavBarImage()
-        }
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self,
-                                                  name: UIDevice.orientationDidChangeNotification,
-                                                  object: nil)
-    }
-    
-    
     // MARK: - View Modifiers
-    
-    func addNavBarImage() {
-        let navController = navigationController!
-        
-        let image = UIImage(named: "HMS.png")
-        
-        let label = UILabel()
-        label.text = "100ms"
-        label.font = .preferredFont(forTextStyle: .title1)
-        label.sizeToFit()
-        label.frame = CGRect(x: navController.navigationBar.frame.size.width/2 - label.frame.size.width/2, y: navController.navigationBar.frame.size.height/2 - label.frame.size.height/2, width: label.frame.size.width, height: label.frame.size.height)
-        
-        
-        let imageView = UIImageView(image: image)
-        let height = navController.navigationBar.frame.height
-        
-        imageView.frame = CGRect(x: label.frame.origin.x - height, y: 0, width: height, height: height)
-        imageView.contentMode = .scaleAspectFit
-        
-        let view = UIView()
-        view.addSubview(label)
-        view.addSubview(imageView)
-        view.frame = navController.navigationBar.bounds
-        
-        navigationItem.titleView = view
-    }
     
     
     // MARK: - Action Handlers
