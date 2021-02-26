1dc6c8120701abd3d3ae146a3da30d3439739180
Yogesh Singh <yogesh@100ms.live>
2021-02-26 14:44:28 +0530

    update nav bar

---
+++ Example/HMSVideoExample/Utilities.swift
@@ -0,0 +1,13 @@
+//
+//  Utilities.swift
+//  HMSVideo_Example
+//
+//  Created by Yogesh Singh on 26/02/21.
+//  Copyright © 2021 100ms. All rights reserved.
+//
+
+import UIKit
+
+class Utilities {
+  
+}
