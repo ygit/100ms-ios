838132a0f0237fba6b0c76758a330d727f4ea0d7
Yogesh Singh <yogesh@100ms.live>
2021-02-26 19:03:23 +0530

    added scrollview on login screen

--- Example/HMSVideoExample/LoginViewController.swift
+++ Example/HMSVideoExample/LoginViewController.swift
@@ -12,10 +12,36 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userNameField: UITextField!
     
     @IBOutlet weak var backgroundView: UIImageView!
+    @IBOutlet weak var stackView: UIStackView! 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     
     // MARK: - View Lifecycle
     
+    override func viewDidLoad() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
     // MARK: - View Modifiers
+    
+    @objc func keyboardWillShow(notification:NSNotification) {
+
+        guard let userInfo = notification.userInfo else { return }
+        var keyboardFrame:CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+
+        var contentInset = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        scrollView.contentInset = contentInset
+    }
+
+    @objc func keyboardWillHide(notification:NSNotification) {
+
+        let contentInset = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+    }
     
     
     // MARK: - Action Handlers
