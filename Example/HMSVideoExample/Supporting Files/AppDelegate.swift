295bea44605ae0374c30fb296e6aa34787a01961
Yogesh Singh <yogesh@100ms.live>
2021-03-04 06:37:43 +0530

    added interface for setting up Firebase

--- Example/HMSVideoExample/Supporting Files/AppDelegate.swift
+++ Example/HMSVideoExample/Supporting Files/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+        FirebaseInteractor()
         return true
     }
 }
---
+++ Example/HMSVideoExample/Supporting Files/FirebaseInteractor.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseInteractor.swift
+//  HMSVideo_Example
+//
+//  Created by Yogesh Singh on 04/03/21.
+//  Copyright © 2021 100ms. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FirebaseInteractor {
+
+    @discardableResult init() {
+        FirebaseApp.configure()
+    }
+}
